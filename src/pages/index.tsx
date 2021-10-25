2f77e590bd34c143e909a263645050bf31d6da88
MaLin <war3_th000@qq.com>
2021-10-25 11:55:47 +0800

    feat：判断是否是移动端

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,27 @@ export default function IndexPage() {
     window.localStorage.setItem('user', data.data.id);
   };
 
+  const isMobile = () => {
+    const agent = navigator.userAgent;
+    const mobileAgents = [
+      'Android',
+      'iPhone',
+      'Symbian',
+      'WindowsPhone',
+      'iPod',
+      'BlackBerry',
+      'Windows CE'
+    ];
+
+    const result = mobileAgents.some((item) => {
+      console.log(item);
+      console.log(agent.indexOf(item) > -1);
+      return agent.indexOf(item) > -1;
+    });
+
+    alert(result);
+  };
+
   const getInfo = () => {
     if (!window.localStorage) {
       alert('浏览器不支持localstorage');
@@ -27,6 +48,7 @@ export default function IndexPage() {
       <h1 className={styles.title}>Page index</h1>
       <Button onClick={login}>登录</Button>
       <Button onClick={getInfo}>获取信息</Button>
+      <Button onClick={isMobile}>是否是移动端</Button>
     </div>
   );
 }
