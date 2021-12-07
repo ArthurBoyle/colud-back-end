0de417709543ab98e9f5b59ea4247454f7e1d5c6
MaLin <war3_th000@qq.com>
2021-12-07 22:04:02 +0800

    feat(Icon)：新增Icon组件

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './index.less';
+
+interface IProps {
+  fontName: string;
+  title: string;
+  onClick: () => void;
+}
+
+const Icon: React.FC<IProps> = (props) => {
+  const { fontName, title, onClick } = props;
+
+  return (
+    <i className={`${style.container} iconfont ${fontName}`} title={title} onClick={onClick} />
+  );
+};
+
+export default Icon;
