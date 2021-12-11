429e9a9fa76780f0a81e71d23c525d66ab237dbf
MaLin <war3_th000@qq.com>
2021-12-11 14:29:15 +0800

    fix(Login)：修改登录组件 1. 抽取登录对话框组件 2. 修改登录逻辑

---
+++ src/pages/Login/components/LoginModal/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useDispatch } from 'umi';
+import { useImmer } from 'use-immer';
+import { Button, Form, Input, message, Modal } from 'antd';
+import { login } from './service';
+import style from './index.less';
+
+const { Item } = Form;
+
+interface IProps {
+  visible: boolean;
+  closeModal: () => void;
+}
+
+const LoginModal: React.FC<IProps> = (props) => {
+  const { visible, closeModal } = props;
+
+  const dispatch = useDispatch();
+  const [form] = Form.useForm();
+
+  const [loading, setLoading] = useImmer(false);
+
+  const handleLogin = async () => {
+    setLoading(true);
+    const params = await form.validateFields();
+    const { status, data } = await login(params);
+    setLoading(false);
+    if (status === 0) {
+      message.warn('账号或密码错误');
+    } else {
+      message.success('登录成功');
+      window.localStorage.setItem('user', data.id);
+      dispatch({
+        type: 'userInfo/getUserInfo'
+      });
+    }
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      title="用户登录"
+      width={370}
+      centered
+      maskClosable={false}
+      wrapClassName={style.loginModal}
+      footer={null}
+      destroyOnClose
+      onCancel={closeModal}
+    >
+      <Form form={form} preserve={false}>
+        <Item label="账号" name="mobile" required={false} rules={[{ required: true }]}>
+          <Input placeholder="请输入账号" />
+        </Item>
+        <Item label="密码" name="pwd" required={false} rules={[{ required: true }]}>
+          <Input.Password placeholder="请输入密码" />
+        </Item>
+      </Form>
+      <Button type="primary" block onClick={handleLogin} loading={loading}>
+        登录
+      </Button>
+    </Modal>
+  );
+};
+
+export default LoginModal;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,25 +1,13 @@
 import React, { useEffect } from 'react';
-import { connect, Dispatch, Loading } from 'umi';
+import { useDispatch } from 'umi';
 import { useImmer } from 'use-immer';
-import { Button, Modal, Form, Input, message } from 'antd';
+import { Button } from 'antd';
 import Footer from './components/Footer';
+import LoginModal from './components/LoginModal';
 import style from './index.less';
 
-const { Item } = Form;
-
-interface IProps {
-  dispatch: Dispatch;
-  loading: boolean | undefined;
-}
-
-interface IState {
-  loading: Loading;
-}
-
-const Login: React.FC<IProps> = (props) => {
-  const { dispatch, loading } = props;
-
-  const [form] = Form.useForm();
+const Login: React.FC = () => {
+  const dispatch = useDispatch();
 
   const [visible, setVisible] = useImmer<boolean>(false);
 
@@ -28,28 +16,6 @@ const Login: React.FC<IProps> = (props) => {
       type: 'userInfo/getUserInfo'
     });
   }, [dispatch]);
-
-  const handleLogin = async () => {
-    const params = await form.validateFields();
-    dispatch({
-      type: 'login/login',
-      payload: params,
-      callback: (result: any) => {
-        if (result) {
-          const { status, data } = result;
-          if (status === 0) {
-            message.warn('账号或密码错误');
-          } else {
-            message.success('登录成功');
-            window.localStorage.setItem('user', data.id);
-            dispatch({
-              type: 'userInfo/getUserInfo'
-            });
-          }
-        }
-      }
-    });
-  };
 
   return (
     <>
@@ -61,7 +27,7 @@ const Login: React.FC<IProps> = (props) => {
             type="primary"
             className={style.loginButton}
             onClick={() => {
-              setVisible(() => true);
+              setVisible(true);
             }}
           >
             登录
@@ -69,35 +35,14 @@ const Login: React.FC<IProps> = (props) => {
         </div>
         <Footer />
       </div>
-      <Modal
+      <LoginModal
         visible={visible}
-        title="用户登录"
-        width={370}
-        centered
-        maskClosable={false}
-        wrapClassName={style.loginModal}
-        footer={null}
-        destroyOnClose
-        onCancel={() => {
-          setVisible(() => false);
+        closeModal={() => {
+          setVisible(false);
         }}
-      >
-        <Form form={form} preserve={false}>
-          <Item label="账号" name="mobile" required={false} rules={[{ required: true }]}>
-            <Input placeholder="请输入账号" />
-          </Item>
-          <Item label="密码" name="pwd" required={false} rules={[{ required: true }]}>
-            <Input.Password placeholder="请输入密码" />
-          </Item>
-        </Form>
-        <Button type="primary" block onClick={handleLogin} loading={loading}>
-          登录
-        </Button>
-      </Modal>
+      />
     </>
   );
 };
 
-export default connect(({ loading }: IState) => ({
-  loading: loading.effects['login/login']
-}))(Login);
+export default Login;
